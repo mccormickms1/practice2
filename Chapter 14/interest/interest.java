dbde0e84e81d3f03b3c1888c33a6389cd02a023a
Matthew McCormick <mccormickms@s.dcsdk12.org>
2018-08-29 09:17:01 -0600

    Practice

---
+++ Chapter 14/interest/interest.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Change {
+
+    public static void main (String[] args){
+        int inData;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("How many pennies ya got:");
+
+        inData = scan.nextInt();
+    }
+}
